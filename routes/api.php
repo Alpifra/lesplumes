508a3dc7b6648762bd170a7c58a8f87d65873076
AC Développement <freelance@alexandrechauvin.fr>
2024-01-05 17:52:28 +0100

    Add: User GET collection and single routes with tests

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Resources\UserCollection;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->name('api.')->group( function() {
+
+    // User resource routing
+    Route::prefix('users')->name('users.')->group( function() {
+        Route::name('collection')->get(
+            '/', fn () => new UserCollection(User::paginate())
+        );
+        Route::name('single')->get(
+            '/{id}', fn (string $id) => new UserResource(User::findOrFail($id))
+        );
+    });
+
 });
---
+++ tests/Feature/Http/Api/Resources/UserApiTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature\Http\Api\Resources;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class UserApiTest extends TestCase
+{
+
+    use WithFaker;
+
+    /**
+     * @test
+     * @group api
+     * @group apiGet
+     * @group user
+     */
+    public function get_user_collection(): void
+    {
+        $user = User::factory()->create(); 
+
+        $response = $this->actingAs($user)
+            ->get('/api/users');
+
+        $response->assertStatus(200)
+            ->assertJson( fn (AssertableJson $json) =>
+                $json->has('meta')
+                     ->has('links')
+                     ->has('data')
+                     ->has(
+                        'data.0',
+                        fn (AssertableJson $json) =>
+                        $json->has('id')
+                             ->has('first_name')
+                             ->has('last_name')
+                             ->has('user_name')
+                             ->has('email')
+                             ->has('created_at')
+                             ->has('updated_at')
+                             ->missing('password')
+                             ->etc()
+                    )
+        );
+    }
+
+    /**
+     * @test
+     * @group api
+     * @group apiGet
+     * @group user
+     */
+    public function get_user(): void
+    {
+        $user = User::factory()->create(); 
+
+        $response = $this->actingAs($user)
+            ->get("/api/users/{$user->id}");
+
+        $response->assertStatus(200)
+            ->assertJson( fn (AssertableJson $json) =>
+                $json->has('data')
+                     ->has(
+                        'data',
+                        fn (AssertableJson $json) =>
+                        $json->has('id')
+                             ->has('first_name')
+                             ->has('last_name')
+                             ->has('user_name')
+                             ->has('email')
+                             ->has('created_at')
+                             ->has('updated_at')
+                             ->missing('password')
+                             ->etc()
+                    )
+        );
+    }
+}
